e42b28f1fd870d75990592df15ed6ef38ac13f10
LahariKemburu <146975432+LahariKemburu@users.noreply.github.com>
2024-04-25 17:19:22 +0530

    Update Nav.jsx

--- src/navigation/Nav.jsx
+++ src/navigation/Nav.jsx
@@ -1,13 +1,18 @@
 import './Nav.css';
 import logo from '../assets/Logo.png';
-import { Link } from "react-router-dom";
-
+import {Link } from "react-router-dom";
 
 export default function Nav() {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      // behavior: 'smooth'
+    });
+  };
   return (
-    <div className="Navbar">
+    <div  className="Navbar">
       <Link to="/"> 
-      <img id="logo" src={logo} alt="Logo" />
+      <img id="logo" src={logo} onClick={scrollToTop} alt="Logo" />
       </Link>
       
       <span className='Home'>
@@ -19,17 +24,19 @@ export default function Nav() {
             <a href="#" className="option">Python Trainings <span>&#9654;</span></a>
             <div className="side-listings">
               <ul>
-                <a href="/python_training_in_banglore"><li>In Banglore </li></a>
-                <a href="/python_training_in_hyderabad"><li>In Hyderabad</li></a>
-                <li>In Vijayawada</li>
+                <Link to="/python_training_in_banglore" onClick={scrollToTop}><li>In Banglore </li></Link>
+                <Link to="/python_training_in_hyderabad" onClick={scrollToTop}><li>In Hyderabad</li></Link>
+                <Link to="/python_training_in_Vijayawada" onClick={scrollToTop} ><li>In Vijayawada</li></Link>
+                
               </ul>
             </div>
             <a href="#" className="option">Python full stack <span>&#9654;</span></a>
             <div className="side-listings">
               <ul>
-                <li>In Banglore </li>
-                <li>In Hyderabad</li>
-                <li>In Vijayawada</li>
+              <Link to="/python-full-stack-developer-in-banglore" onClick={scrollToTop}><li>In Banglore</li></Link>
+              <Link to="/python-full-stack-developer-in-hyderabad" onClick={scrollToTop}><li>In Hyderabad</li></Link>
+              <Link to="/python-full-stack-developer-in-vijayawada" onClick={scrollToTop}> <li>In Vijayawada</li></Link>
+             
               </ul>
             </div>
             <a href="#" className="option">Java Training  <span>&#9654;</span></a>
@@ -92,8 +99,8 @@ export default function Nav() {
             <a href="#" className="option">Syllabus <span>&#9654;</span></a>
             <div className="side-listings">
               <ul>
-                <a href="/java_course_page"><li>Java Syllabus</li></a>
-                <a href="/ds_course_page"><li>Data structures</li></a>
+                <a href="./java_course_page"><li>Java Syllabus</li></a>
+                <a href="./ds_course_page"><li>Data structures</li></a>
                 <li>Python syllabus</li>
                 <li>React JS syllabus</li>
                 <li>C language syllabus</li>
@@ -104,15 +111,13 @@ export default function Nav() {
           </div>
         </div>
 
-
-{/*         <a className="dropbtn" href="/placements">Placements</a> */}
-         <Link className="dropbtn" to="/placements" onClick={scrollToTop}>Placements</Link>
+        <Link className="dropbtn" to="/placements" onClick={scrollToTop}>Placements</Link>
 
         <div className="dropdown">
           <button className="dropbtn">About <span className='down_triangle'>&#9660;</span></button>
           <div className="dropdown-content">
-            <a href="/about" className="option">Team </a>
-            <a href="/gallery" className="option">Gallery</a>
+            <Link to="/about" className="option" onClick={scrollToTop}>Team </Link>
+            <Link to="/gallery" className="option" onClick={scrollToTop}>Gallery</Link>
             <a href="#" className="option">Internships</a>
             <a href="#" className="option">Job Accelerator Program</a>
           </div>
